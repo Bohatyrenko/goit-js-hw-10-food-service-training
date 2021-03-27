34e34c17228503d059e8b9590e8e0f2f27cbd2b5
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-03-28 00:35:02 +0200

    Добавляет карточки меню в разметку

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import './styles.css';
-// import svgSprite from './images/sprite.svg';
-import './js/shared-folder';
+// import './js/shared-folder';
 import './js/menu';
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,12 +1,59 @@
 import menu from './menu.json';
-import cardMenuTemplate from '../templates/card-menu.hbs';
-import '../css/menu.css';
-
-console.log(menu);
+import cardsMenuTemplate from '../templates/cards-list.hbs';
 
 const listMenuRef = document.querySelector('.js-menu');
+const checkboxRef = document.querySelector('.theme-switch__track');
+const bodyRef = document.querySelector('body');
 
-const menuTemplate = menu.map(cardMenu => cardMenuTemplate(cardMenu)).join('');
-console.log(menuTemplate);
+checkboxRef.addEventListener('click', changeTheme);
 
-listMenuRef.innerHTML = menuTemplate;
+listMenuRef.innerHTML = cardsMenuTemplate(menu);
+
+bodyRef.classList.add('light-theme');
+
+// const valueTheme = localStorage.getItem('Theme');
+// console.log(valueTheme);
+
+// function changeClass(oldClass, newClass) {
+//   bodyRef.classList.remove(oldClass);
+//   bodyRef.classList.add(newClass);
+// }
+
+// const onDarkTheme = () => {
+//   changeClass(dark - theme, light - theme);
+//   localStorage.setItem('Theme', dark - theme);
+//   checkboxRef.setAttribute('checked', true);
+// };
+
+// const onLightTheme = () => {
+//   changeClass(light - theme, dark - theme);
+//   localStorage.setItem('Theme', light - theme);
+// };
+
+// function checkboxFn() {
+//   if (bodyRef.classList.contains(light - theme)) {
+//     onDarkTheme();
+//   } else {
+//     onLightTheme();
+//   }
+// }
+// if (valueThem === Theme.dark - theme) {
+//   onDarkTheme();
+// }
+
+function changeTheme(e) {
+  const darkTheme = bodyRef.classList.add('dark-theme');
+  if (bodyRef.classList === 'dark-theme') {
+    bodyRef.replace('dark-theme', 'light-theme');
+  }
+}
+
+//   const value = e.currentTarget.value;
+//   console.log(value);
+//   localStorage.setItem('currentTheme', chengeTheme(e));
+// }
+// const saveData = localStorage.getItem('my-data');
+// console.log(saveData);
+// const parsedData = JSON.parse(saveData);
+// console.log(parsedData);
+// localStorage.setItem('my-data', JSON.stringify({ name: 'Mango', age: 5 }));
